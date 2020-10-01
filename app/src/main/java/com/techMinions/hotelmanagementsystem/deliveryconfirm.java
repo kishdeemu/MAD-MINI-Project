cbb6e1804fad4fae449625db7ae1aac3bd932ee7
KosalaDK <kosaladisanayake.dk@gmail.com>
2020-10-01 10:58:02 +0530

    create,read of delivery

--- app/src/main/java/com/techMinions/hotelmanagementsystem/deliveryconfirm.java
+++ app/src/main/java/com/techMinions/hotelmanagementsystem/deliveryconfirm.java
@@ -1,26 +1,76 @@
 package com.techMinions.hotelmanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class deliveryconfirm extends AppCompatActivity {
     Button updateBtn;
     Button deleteBtn;
     Button confirmBtn;
 
+    TextView name,email,phone,quantity,address,fooditm,ncity,total;
+    DatabaseReference dbref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliveryconfirm);
 
+        name = findViewById(R.id.cfs_name);
+        email = findViewById(R.id.cfs_email);
+        phone = findViewById(R.id.cfs_phone);
+        quantity = findViewById(R.id.cfs_quantity);
+        address = findViewById(R.id.cfs_deliveryaddress);
+        fooditm = findViewById(R.id.cfs_fooditem);
+        ncity = findViewById(R.id.cfs_nearcity);
+        total = findViewById(R.id.cfs_totalprice);
+
         updateBtn = (Button) findViewById(R.id.deliodrupdtBtn);
         deleteBtn = (Button) findViewById(R.id.deliodrdelBtn);
         confirmBtn = (Button) findViewById(R.id.deliodrcfBtn);
 
+        dbref = FirebaseDatabase.getInstance().getReference().child("Delivery").child("lastDeliveryData");
+
+        dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    Log.d("deliveryread", String.valueOf(dataSnapshot));
+                    name.setText(dataSnapshot.child("name").getValue().toString());
+                    email.setText(dataSnapshot.child("email").getValue().toString());
+                    phone.setText(dataSnapshot.child("phone").getValue().toString());
+                    quantity.setText(dataSnapshot.child("quantity").getValue().toString());
+                    address.setText(dataSnapshot.child("address").getValue().toString());
+                    fooditm.setText(dataSnapshot.child("fooditm").getValue().toString());
+                    ncity.setText(dataSnapshot.child("city").getValue().toString());
+                    total.setText(dataSnapshot.child("totalprice").getValue().toString());
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "No source to Display", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/techMinions/hotelmanagementsystem/deliveryorder.java
+++ app/src/main/java/com/techMinions/hotelmanagementsystem/deliveryorder.java
@@ -4,20 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class deliveryorder extends AppCompatActivity {
     Spinner foditm;
     Spinner ncity;
     Button nextBtn;
+    delivery_model delimodel;
+    EditText name,email,phone,quantity,address;
+    DatabaseReference dbref;
+    int totalprice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliveryorder);
+
+        name        = findViewById(R.id.edittxtName);
+        email       = findViewById(R.id.edittxtEmail);
+        phone       = findViewById(R.id.edittxtPhone);
+        quantity    = findViewById(R.id.edittxtQuantity);
+        address     = findViewById(R.id.edittxtAddress);
+
+        delimodel = new delivery_model();
+
 
         foditm = findViewById(R.id.fooditemlist);
         ArrayAdapter<String> fditm = new ArrayAdapter<>(deliveryorder.this,
@@ -35,6 +54,55 @@ public class deliveryorder extends AppCompatActivity {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+                dbref = FirebaseDatabase.getInstance().getReference().child("Delivery");
+                try{
+                    if(TextUtils.isEmpty(name.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(email.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(foditm.getSelectedItem().toString()))
+                        Toast.makeText(getApplicationContext(), "Please select a food item", Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(phone.getText().toString()))
+                       Toast.makeText(getApplicationContext(), "Please enter your phone number", Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(quantity.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter quantity", Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(address.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter a delivery address", Toast.LENGTH_SHORT).show();
+                    //else if(TextUtils.isEmpty(ncity.getSelectedItem().toString()))
+                        //Toast.makeText(getApplicationContext(), "Please select a near city", Toast.LENGTH_SHORT).show();
+                    else{
+                        if(foditm.getSelectedItem().toString().equals("Rice And Curry")){
+                            totalprice = 250 * Integer.parseInt(quantity.getText().toString());
+                        }else if(foditm.getSelectedItem().toString().equals("Fried Rice")){
+                            totalprice = 350 * Integer.parseInt(quantity.getText().toString());
+                        }else if(foditm.getSelectedItem().toString().equals("Koththu")){
+                            totalprice = 400 * Integer.parseInt(quantity.getText().toString());
+                        }else if(foditm.getSelectedItem().toString().equals("Hoppers")){
+                            totalprice = 30 * Integer.parseInt(quantity.getText().toString());
+                        }
+
+                        delimodel.setName(name.getText().toString().trim());
+                        delimodel.setEmail(email.getText().toString().trim());
+                        delimodel.setFooditm(foditm.getSelectedItem().toString().trim());
+                        delimodel.setPhone(phone.getText().toString().trim());
+                        delimodel.setQuantity(quantity.getText().toString().trim());
+                        delimodel.setAddress(address.getText().toString().trim());
+                        delimodel.setCity(ncity.getSelectedItem().toString().trim());
+                        delimodel.setTotalprice(totalprice);
+
+                        dbref.push().setValue(delimodel);
+
+                        dbref.child("lastDeliveryData").setValue(delimodel);
+
+                        Toast.makeText(getApplicationContext(), "Data Saved Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }catch(NumberFormatException e){
+                    Toast.makeText(getApplicationContext(), "Invalid contact number", Toast.LENGTH_SHORT).show();
+
+                }
+
                 Intent nextintent = new Intent(deliveryorder.this,deliveryconfirm.class);
                 startActivity(nextintent);
             }
